d2a6a6472cbc0482e9129a5fa9973d9e24a6f70f
godxiaoji <godxiaoji@vip.qq.com>
2013-03-25 09:22:57 +0800

    add new

---
+++ Readme.md
@@ -0,0 +1,27 @@
+# tSlide
+
+jQuery简单实用滑动幻灯片代码
+
+### Usage
+    
+    <script type="text/javascript">
+        $(function(){
+            $("#tSlide").tSlide({time:4000});
+        });
+    </script>
+    
+### Options
+    
+* `time`: 幻灯片切换时间 (Default: `4000`)
+* `overtime`: 幻灯片切换过程中的过度时间 (Default: `200`)
+* `opacity`: 导航遮罩透明度 (Default: `0.5`)
+* `classcur`: 焦点类名 (Default: `current`)
+* `classcon`: 图片UL类名 (Default: `t_slide_con`)
+* `classmask`: 导航遮罩类名 (Default: `t_slide_mask`)
+* `classtitle`: 标题UL类名 (Default: `t_slide_title`)
+* `classpage`: 分页UL类名 (Default: `t_slide_page`)
+
+### Author
+
+[Travis](http://travisup.com/)
+
---
+++ js/jquery.tslide.js
@@ -0,0 +1,67 @@
+/*
+ * jQuery tSlide
+ * @Version: 1.0
+ * @Author:  Travis
+ * @Date:    October 10th, 2012
+ * @Website: http://travisup.com/
+ */
+(function($) {
+    $.fn.extend({
+        tSlide:init
+    });
+    
+    var opt = {
+        time:4000,
+        overtime: 200,
+        opacity: '0.5',
+        mouseevent: 'mouseover',
+        classcur: 'current',
+        classcon: 't_slide_con',
+        classmask: 't_slide_mask',
+        classtitle: 't_slide_title',
+        classpage: 't_slide_page'
+    };
+    
+    var $obj = {};
+    
+    var current = 0;
+    
+    function slide() {
+        current = (current+1) % opt.len;
+        var left = 0 - current * opt.width;
+        $obj.con.stop().animate({left:left}, opt.overtime);
+        $obj.title.removeClass(opt.classcur).eq(current).addClass(opt.classcur);
+        $obj.page.removeClass(opt.classcur).eq(current).addClass(opt.classcur);
+    };
+    
+    function init(options) {
+        if(options) $.extend(opt, options);
+        
+        $obj.con = $(this).find('.'+opt.classcon).find("ul");
+        $obj.mask = $(this).find('.'+opt.classmask);
+        $obj.title = $(this).find('.'+opt.classtitle).find("li");
+        $obj.page = $(this).find('.'+opt.classpage).find("li");
+        
+        opt.width = $(this).find('.'+opt.classcon).width();
+        opt.len = $obj.page.length;
+        
+        $obj.mask.css({'opacity':opt.opacity});
+        $obj.title.eq(0).addClass(opt.classcur);
+        $obj.page.eq(0).addClass(opt.classcur);
+
+        var timer = setInterval(slide, opt.time);
+
+        $(this).hover(function() {
+            clearInterval(timer);
+        }, function() {
+            timer = setInterval(slide, opt.time);
+        });
+        
+        $obj.page.each(function(i){
+            $(this).bind(opt.mouseevent, function(){
+                current = i - 1;
+                slide();
+            });
+        });
+    }
+})(jQuery);
